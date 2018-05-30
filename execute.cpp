a51f4d55b613881902dbf861461f553fe899951c
imanewman <tjnewman111@gmail.com>
2018-05-29 20:22:12 -0700

    Add files via upload

--- execute.cpp
+++ execute.cpp
@@ -732,9 +732,6 @@ void execute() {
       temp = imem[addr] | (imem[addr+2]<<16);  // temp is a Data32
       rf.write(ldrl.instr.ldrl.rt, temp);
 
-      //caches.access(addr);
-      //caches.access(addr+2);
-
       // One write for updated reg
       stats.numRegWrites++;
       // One read of the PC
